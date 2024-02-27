1fd20dfe81d884411bb695f0ddb7e4ea7ee069c7
chevyuxu <miyulll92@gmail.com>
2024-02-27 23:59:28 +0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function getLocation() {
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(showPosition);
+            } else {
+                document.getElementById("demo").innerHTML = "Geolocation is not supported by this browser.";
+            }
+        }
+
+        function showPosition(position) {
+            var latitude = position.coords.latitude;
+            var longitude = position.coords.longitude;
+            document.getElementById("demo").innerHTML = "Latitude: " + latitude + "<br>Longitude: " + longitude;
+        }
